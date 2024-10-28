1767e787355d266fa57d9d5e62c730d588e19610
MarcusFC3 <22795@my4county.net>
2024-10-28 13:52:46 -0400

    added client folder layout

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,42 @@
+const Navbar = () => {
+    return <div class="row">
+        <div class="col-12">
+            <nav class="navbar navbar-expand-lg navbar-light navbar-custom">
+                <div class="container-fluid">
+                    <a class="navbar-brand" aria-disabled="true"><span id="navbarBrand">
+                        <img src="logo.svg" alt="Simply Health Logo" height="35"></img>
+                    </span></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="activities.html">Activities</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="leaderboards.html">leaderboards</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="login.html">Login</a></li>
+                                    <li></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+}
+export default Navbar
---
+++ client/src/pages/home.js
@@ -0,0 +1,26 @@
+const Home = () => {
+    return <div>
+        <div class="row">
+            <div class="col-12">
+                <div class="homebackgroundimage">
+                    <div class="homebackgroundimagetext">Welcome</div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <h2>Habit Tracking Made Easy</h2>
+                <p>Desc</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h2>Teams</h2>
+                <p>Desc</p>
+            </div>
+            <div class="col-12 col-md-4">
+                <h2>Goals</h2>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</div >
+}
