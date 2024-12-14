e558841cbbf62f7c10bbdfeca0a0071e24fdc4fa
Marcus Jones Jr. <22795@my4county.net>
2024-12-14 12:55:07 -0500

    added conectivity to azure cloud database. added outline of future functions. fixed multiple function in login and activites controller

--- server/src/models/dbqueries.js
+++ server/src/models/dbqueries.js
@@ -71,8 +71,8 @@ const get = {
 const check = {
     IfUserExists: async function checkIfUserExists(email) {
         const connectionPool = await sql.connect(adminconf);
-        let request = await connectionPool.request();
-        request = await request.input("email", sql.VarChar, `${email}`);
+        let request = connectionPool.request();
+        request = request.input("email", sql.VarChar, `${email}`);
         return await request.query("SELECT email FROM Users WHERE email = @email")
     },
 
@@ -84,8 +84,8 @@ const check = {
     },
     IfTeamExists: async function checkIfTeamExists(teamName) {
         const connectionPool = await sql.connect(adminconf);
-        let request = await connectionPool.request();
-        request = await request.input("teamName", sql.VarChar, `${teamName}`);
+        let request =  connectionPool.request();
+        request =  request.input("teamName", sql.VarChar, `${teamName}`);
         return await request.query("SELECT TeamName FROM Teams WHERE TeamName = @teamName")
     },
     ForMatchingPassword: async function checkformatch(email) {
@@ -99,9 +99,24 @@ const check = {
 }
 const add = {
     UserToDbCreateCompanyAndTeam: async function addUserToDbWithTeam(firstName, lastName, username, email, password, teamName, companyName) {
-        const connection = await sql.connect(adminconf);
-        const request = connection.request();
+       
+        const connection = await sql.connect(adminconf).then(() => console.log('Connected to Azure SQL Database'))
+        .catch(err => console.error('Connection failed:', err));;
+        console.log("asdasddddddddddddddddd")
+        check.IfTeamExists(teamName).then((result) => {
+            console.log(JSON.stringify(result))
+            console.log(JSON.stringify(result))
+            if (result.recordset[0]) {
+                error = new Error({
+                    status: "Failure",
+                    message: "This Team Already Exists"
+                })
+
+
+            }
+        }).catch((err)=>{console.log("asdasddddddddddddddddd")})
         //Generate salt seperatly and store it in the database along with the password
+        console.log("asdasddddddddddddddddd")
         const salt = await new Promise((resolve, reject) => {
             bcrypt.genSalt(10, (err, salt) => {
                 if (err) {
@@ -123,22 +138,11 @@ const add = {
                 }
             });
 
-        },
-        )
-        let error = undefined;
-
-        check.IfTeamExists(teamName).then((result) => {
-            console.log(JSON.stringify(result))
-            console.log(JSON.stringify(result))
-            if (result.recordset[0]) {
-                error = new Error({
-                    status: "Failure",
-                    message: "This Team Already Exists"
-                })
-
-
-            }
         })
+        let error = undefined;
+        console.log("asdasddddddddddddddddd")
+        
+        console.log("asdasddddddddddddddddd")
         check.IfCompanyExists(companyName).then(async (result) => {
             if (result.recordset[0]) {
                 error = new Error({
@@ -154,22 +158,23 @@ const add = {
         if (error){
             throw error
         } else{
+            console.log("asdasddddddddddddddddd")
 
-        
-
+      
 /**
  * Transaction keeps executing SQL code even with error.
  */
 
 
-const transaction = connection.transaction(connection);
+const transaction = new sql.Transaction(connection)
+console.log("DONe!")
 try {
     
     await transaction.begin();
     const tRequest = transaction.request();
     
     console.log("Transaction started");
-
+    
     // Inserting Company
     tRequest.input("companyName", sql.VarChar, companyName);
     await tRequest.query("INSERT INTO Companies (CompanyName) VALUES (@companyName)");
@@ -207,7 +212,8 @@ try {
     console.log("Transaction committed");
 
 } catch (err) {
-        console.log(err)
+    console.log("Asasdad")
+        console.log(err + "   asc")
         // Rollback the transaction in case of error
         await transaction.rollback()
         console.log("Transaction rolled back due to error");
@@ -228,8 +234,10 @@ try {
             
         
     }
-}
-,
+
+
+},
+
     userToDbAddToCompanyAndTeam: async function addUserToDbNoTeam(firstName, lastName, username, email, password, teamName, companyName) {
         const connection = await sql.connect(adminconf);
         const request = connection.request();
@@ -274,9 +282,40 @@ try {
         return await request.query("INSERT INTO Users (firstName,lastName, username, email, hashedPassword, TeamName, CompanyID) VALUES (@firstName, @lastName, @username, @email, @password, @teamName, @companyID)");
     }
 }
-
+const update = {
+    resetPassword : async function resetPassword(password, email){
+        const salt = await new Promise((resolve, reject) => {
+            bcrypt.genSalt(10, (err, salt) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    console.log("" + salt)
+                    resolve(salt)
+                }
+            })
+        })
+        const hashedpassword = await new Promise((resolve, reject) => {
+    
+            bcrypt.hash(password, salt, (err, hash) => {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(hash)
+                }
+            });
+    
+        })
+        const connection = await sql.connect(adminconf);
+        const request = connection.request();
+        request.input("newpassword", sql.VarChar, hashedpassword)
+        request.input("email", sql.VarChar, email)
+        return await request.query("UPDATE Users SET hashedpassword = @newpassword WHERE email = @email")
+    }
+}
 module.exports = {
     get,
     check,
-    add
+    add,
+    update
 }
