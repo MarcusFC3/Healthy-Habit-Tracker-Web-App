e6a396a0eaea598d09781ebd32e3f3136bc5706a
Lukas Pribe <lukaspribe@gmail.com>
2024-12-15 11:39:33 -0500

    Making a form to let the user create a form

--- client/src/components/Activity.jsx
+++ client/src/components/Activity.jsx
@@ -9,20 +9,6 @@ export default function Activity(props) {
                 <h2>{props.Name}</h2>
                 <p>{props.descr}</p>
                 <div className="progress-bar-holder"><div style={style} className="progress_bar"></div></div>
-                <button onClick={null}>Edit Activity</button>
-
-
-                            <div className="editActivity dropdown">
-                                <h2 className="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    Edit Activity
-                                </h2>
-                                <p className="dropdown-menu">
-
-                                </p>
-                            </div>
-
-
                 <button onClick={props.delete}>Delete Activity</button>
         </div>
     )
--- client/src/pages/activities.js
+++ client/src/pages/activities.js
@@ -2,6 +2,8 @@
 import { useState } from 'react'
 import Activity from "../components/Activity";
 
+let activityCount = 1
+
 const Activities = () => {
     const [activities, setActivities] = useState(generateActivity())
 
@@ -16,18 +18,32 @@ const Activities = () => {
     console.log(activities)
 
     function createNewActivity(activityKey, activityName, activityDescr) {
+        activityCount++
         setActivities(prevActivities => [
             ...prevActivities, {
                 key: activityKey,
                 Name: activityName,
                 descr: activityDescr }
         ])
+        console.log(activities)
     }
 
     function deleteActivity(activityKey) {
-        setActivities(prevActivities => [
-            prevActivities.splice(activityKey, 1)
-        ])
+        setActivities(prevActivities => {
+            prevActivities.splice(prevActivities[activityKey], 1)
+            return [
+            ...prevActivities,
+            ]
+    })
+
+        console.log(activities)
+    }
+
+    function editActivity(activityKey) {
+
+        setActivities(prevActivities => {
+            prevActivities[activityKey] = {}
+        })
     }
 
     const activityElements = activities.map(activityObj => 
@@ -38,11 +54,31 @@ const Activities = () => {
         delete={() => deleteActivity(activityObj.key)}
         />)
 
+        // Create an open form function
+
     return <div>
         <div className="row">
             <h1>Activities</h1>
 
-            <button onClick={() => { createNewActivity(1, "Activity 1", "This is a test activity")}}>Create Activity</button>
+            <button onClick={() => {createNewActivity(activityCount, "Activity " + activityCount, "This is a test activity")}}>Create Activity</button>
+
+            <div id="activity_form_container" className="activityForm">
+                <form  id="activity_form" onSubmit={null}>
+                    <h1>Create an Activity</h1>
+
+                    <label>Activity Name</label>
+                    <input id="acitivity_name" name="activityName" type="text"></input>
+
+                    <label>Activity Description</label>
+                    <input id="acitivity_descr" name="activityDescr" type="text"></input>
+
+                    <label>Activity Progress</label>
+                    <input id="acitivity_progress" name="activityProgress" type="text"></input>
+
+                    <button onClick={null}>Create Activity</button>
+                    <button type="button" className="btn cancel" onClick={null}>Close</button>
+                </form>
+            </div>
 
             <div className="activity-container">
                 {activityElements}
