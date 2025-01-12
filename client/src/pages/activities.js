6d90ac1142b44dd57bf541aa81f885dcd5e563ac
Marcus Jones Jr. <22795@my4county.net>
2025-01-12 14:07:06 -0500

    testing leaderboard and activity get requests

--- client/src/pages/activities.js
+++ client/src/pages/activities.js
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 import Activity from "../components/Activity";
 
 import { postActivityData, getActivityData } from '../hooks/requests';
--- client/src/pages/leaderboards.js
+++ client/src/pages/leaderboards.js
@@ -1,5 +1,5 @@
 
-import { useState } from "react"
+import { useState, useEffect } from 'react';
 import LeaderboardRow from "../components/LeaderboardRow"
 
 import { getForLeaderboard } from "../hooks/requests"
